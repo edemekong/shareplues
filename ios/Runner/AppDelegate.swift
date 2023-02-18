72ed0f273375db162dfca28b0e78538f9fbc97ba
Paul Edemekong <hello.paulvin@gmail.com>
2023-02-18 02:38:54 +0100

    setup project for course intro

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,5 @@
 import UIKit
 import Flutter
-import Firebase
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,12 +7,7 @@ import Firebase
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    FirebaseApp.configure()
     GeneratedPluginRegistrant.register(with: self)
-    if #available(iOS 11.0, *) {
-      UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
-    }
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
-
--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,7 @@
-import 'dart:async';
-
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:shareplues/services/navigation_service.dart';
 
 void main() {
-  runZonedGuarded<Future<void>>(() async {
-    WidgetsFlutterBinding.ensureInitialized();
-    await Firebase.initializeApp();
-    runApp(const ProviderScope(
-      child: SharepluesApp(),
-    ));
-  }, (error, stack) => print(error));
-}
-
-class SharepluesApp extends ConsumerWidget {
-  const SharepluesApp({super.key});
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final state = ref.read(navigationServiceProvider);
-
-    return MaterialApp.router(
-      title: 'Shareplues',
-      builder: (context, widget) => Overlay(
-        initialEntries: [
-          OverlayEntry(
-            builder: (context) => widget!,
-          ),
-        ],
-      ),
-      routerConfig: state.router,
-      shortcuts: {
-        LogicalKeySet(LogicalKeyboardKey.space): const ActivateIntent(),
-      },
-    );
-  }
+  runApp(MaterialApp(
+    home: Scaffold(),
+  ));
 }
